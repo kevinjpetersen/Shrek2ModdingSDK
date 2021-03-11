4d886ecc395360cd151115d2883a3bb25488421b
Kevin Petersen <kevingeeken@gmail.com>
2021-03-11 05:16:28 +0100

    Changes

---
+++ Shrek2ModdingSDK/Shrek2CETConverter/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Shrek2CETConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextbox = new System.Windows.Forms.TextBox();
+            this.outputTextbox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.Location = new System.Drawing.Point(13, 13);
+            this.inputTextbox.MaxLength = 0;
+            this.inputTextbox.Multiline = true;
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.Size = new System.Drawing.Size(406, 135);
+            this.inputTextbox.TabIndex = 0;
+            // 
+            // outputTextbox
+            // 
+            this.outputTextbox.Location = new System.Drawing.Point(425, 13);
+            this.outputTextbox.MaxLength = 0;
+            this.outputTextbox.Multiline = true;
+            this.outputTextbox.Name = "outputTextbox";
+            this.outputTextbox.Size = new System.Drawing.Size(375, 135);
+            this.outputTextbox.TabIndex = 1;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(13, 154);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(787, 41);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(812, 207);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.outputTextbox);
+            this.Controls.Add(this.inputTextbox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Shrek 2 Cheat Engine Table Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextbox;
+        private System.Windows.Forms.TextBox outputTextbox;
+        private System.Windows.Forms.Button convertButton;
+    }
+}
+
---
+++ Shrek2ModdingSDK/Shrek2CETConverter/Form1.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shrek2CETConverter
+{
+    public partial class Form1 : Form
+    {
+        class SCheatOffsets
+        {
+            public List<object> Offset { get; set; }
+        }
+
+        class SCheatRoot
+        {
+            public SCheatTable CheatTable { get; set; }
+        }
+
+        class SCheatTable
+        {
+            public SCheatEntries CheatEntries { get; set; }
+        }
+
+        class SCheatEntries
+        {
+            public List<SCheatEntry> CheatEntry { get; set; }
+        }
+
+        class SCheatEntry
+        {
+            public int ID { get; set; }
+            public string Description { get; set; }
+            public string VariableType { get; set; }
+            public string Address { get; set; }
+            public SCheatOffsets Offsets { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string GetReturnType(string type)
+        {
+            switch (type)
+            {
+                case "Float": return "float";
+                case "2 Bytes": return "short int";
+                case "4 Bytes": return "int";
+                case "Byte": return "bool";
+                default: return "UNKNOWN_TYPE";
+            }
+        }
+
+        string GetMemoryFunctionType(string type)
+        {
+            switch (type)
+            {
+                case "Float": return "Float";
+                case "2 Bytes": return "ShortInt";
+                case "4 Bytes": return "Int";
+                case "Byte": return "Bool";
+                default: return "UNKNOWN_TYPE";
+            }
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(inputTextbox.Text)) return;
+
+            try
+            {
+                var ct = JsonConvert.DeserializeObject<SCheatRoot>(inputTextbox.Text);
+
+                var headerPointerCodes = new List<string>();
+                var sourcePointerCodes = new List<string>();
+
+                foreach(var ce in ct.CheatTable.CheatEntries.CheatEntry)
+                {
+                    if(ce.Description.Contains("T)"))
+                    {
+
+                    } else
+                    {
+                        ce.Description = ce.Description.Replace(" ", "").Replace("\"", "");
+                        ce.Offsets.Offset.Reverse();
+
+                        var baseAddr = ce.Address.Split('+')[0];
+                        var addr = ce.Address.Split('+')[1];
+
+                        string header = @"
+" + GetReturnType(ce.VariableType) + " Get" + ce.Description + @"();
+bool Set" + ce.Description + "(" + GetReturnType(ce.VariableType) + " " + ce.Description + @")";
+
+                        string source = @"
+bool Shrek2Pointers::Set" + ce.Description + "(" + GetReturnType(ce.VariableType) + " " + ce.Description + @") {
+	return Shrek2Memory::Write" + GetMemoryFunctionType(ce.VariableType) + "("+ ce.Description + ", " + baseAddr + ", " + addr + ", 0x" + ce.Offsets.Offset[0].ToString() + ", 0x" + ce.Offsets.Offset[1].ToString() + ", 0x" + ce.Offsets.Offset[2].ToString() + ", 0x" + ce.Offsets.Offset[3].ToString() + @");
+}
+" + GetReturnType(ce.VariableType) + " Shrek2Pointers::Get" + ce.Description + @"() {
+    return Shrek2Memory::Read" + GetMemoryFunctionType(ce.VariableType) + "(" + ce.Description + ", " + baseAddr + ", " + addr + ", 0x" + ce.Offsets.Offset[0].ToString() + ", 0x" + ce.Offsets.Offset[1].ToString() + ", 0x" + ce.Offsets.Offset[2].ToString() + ", 0x" + ce.Offsets.Offset[3].ToString() + @");
+}";
+
+                        headerPointerCodes.Add(header);
+                        sourcePointerCodes.Add(source);
+                    }
+
+                    outputTextbox.Text = string.Join("\r\n", headerPointerCodes);
+                    outputTextbox.Text += "\r\n\r\n";
+                    outputTextbox.Text += string.Join("\r\n", sourcePointerCodes);
+
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Invalid Cheat Table JSON");
+            }
+        }
+    }
+}
