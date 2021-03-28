e20b6f4a2296147c5a2937959aaf39e8797cf378
Kevin Petersen <kevingeeken@gmail.com>
2021-03-28 21:55:02 +0200

    Update dllmain.cpp

--- Shrek2ModdingSDK/Shrek2TestMod/dllmain.cpp
+++ Shrek2ModdingSDK/Shrek2TestMod/dllmain.cpp
@@ -43,35 +43,47 @@ void OnTick()
 			auto entities = LoadEntities(addr);
 			if (entities.size() > 0)
 			{
+				Game.LogToConsole("Amount of Entities in Level: " + std::to_string(entities.size()));
+
 				for (Entity* ent : entities)
 				{
-					std::string name1 = "Unknown 1";
-					std::string name2 = "Unknown 2";
+					std::string name1 = "";
+					std::string name2 = "";
 
 					if (ent->Name1) {
 						std::wstring str = reinterpret_cast<wchar_t*>(ent->Name1);
-						
+
 						if (!str.empty())
 						{
 							name1 = Shrek2Utils::WS2String(str);
 						}
+					}
 
+					if (ent->Name2) {
+
+						MEMORY_BASIC_INFORMATION mbi;
+						VirtualQuery(ent->Name2, &mbi, sizeof(mbi));
+						
+
+						if (!(mbi.Protect & PAGE_GUARD | PAGE_NOACCESS))
+						{
+							Game.LogToConsole("Blablabla");
+
+							std::wstring str = reinterpret_cast<wchar_t*>(ent->Name2);
+
+							if (!str.empty())
+							{
+								name2 = Shrek2Utils::WS2String(str);
+							}
+						}
+					}
+
+					if (!name1.empty()) {
 						std::cout << "Name 1: " << name1 << std::endl;
 						//std::cout << "Name 2: " << name2 << std::endl;
 						std::cout << "X: " << ent->Position.x << ", Y: " << ent->Position.y << ", Z: " << ent->Position.z << std::endl;
 						std::cout << "----" << std::endl;
 					}
-
-					/*if (ent->Name2) {
-						std::wstring str = reinterpret_cast<wchar_t*>(ent->Name2);
-
-						if (!str.empty())
-						{
-							name2 = Shrek2Utils::WS2String(str);
-						}
-					}*/
-
-					
 				}
 			}
 			else {
