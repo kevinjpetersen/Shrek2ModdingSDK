fec072bf8a08dd10df860fa13150b4ff2d8af1d2
Kevin J. Petersen <kevingeeken@gmail.com>
2021-10-25 04:36:45 +0200

    Init of new Shrek 2 Mod Manager

---
+++ Shrek2ModdingSDK/Shrek2ModManager/Models/Shrek2ModListItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shrek2ModManager
+{
+    public class Shrek2ModListItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _IsChecked;
+        public bool IsChecked
+        {
+            get => _IsChecked;
+            set
+            {
+                if (_IsChecked != value)
+                {
+                    _IsChecked = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsChecked)));
+                }
+            }
+        }
+
+        public string Title { get; }
+        public string Description { get; }
+
+        public Shrek2ModListItem(string title, string description = "No description found.")
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
---
+++ Shrek2ModdingSDK/Shrek2ModManager/Shrek2DataContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shrek2ModManager
+{
+    public class Shrek2DataContext
+    {
+        public ObservableCollection<Shrek2ModListItem> Items { get; } = new ObservableCollection<Shrek2ModListItem>();
+
+        public Shrek2DataContext()
+        {
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    Items.Add(new Shrek2ModListItem($"Item {i}"));
+            //}
+
+
+            Items.Add(new Shrek2ModListItem($"Backflip Mod", "This will allow you to backflip when jumping."));
+            Items.Add(new Shrek2ModListItem($"Better Ghost Mod", "This adds a better ghosting mechanic."));
+            Items.Add(new Shrek2ModListItem($"MasterToolz"));
+            Items.Add(new Shrek2ModListItem($"Chaos Mod v2"));
+            Items.Add(new Shrek2ModListItem($"Randomizer Mod"));
+            Items.Add(new Shrek2ModListItem($"Infinite Coins Mod"));
+            Items.Add(new Shrek2ModListItem($"Replay Mod"));
+        }
+    }
+}
